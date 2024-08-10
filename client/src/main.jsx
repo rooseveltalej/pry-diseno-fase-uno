e5975360d2c1deff85e9d1c79205d9ae72b76e12
FabsCR <fabfernandez@estudiantec.com>
2024-08-10 00:37:40 -0600

    fix: font color fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import LoginForm from './components/login/LoginForm.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
